f3bc9e6d9be66f85f332772f1d946984d58bb965
kakaba2009 <_@example.com>
2023-04-13 21:51:24 +0900

    Add more files

--- String/string.go
+++ String/string.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	s "strings"
+	"strings"
 )
 
 var p = fmt.Println
 
 func main() {
-	p("Contains:  ", s.Contains("test", "es"))
-	p("Count:     ", s.Count("test", "t"))
-	p("HasPrefix: ", s.HasPrefix("test", "te"))
-	p("HasSuffix: ", s.HasSuffix("test", "st"))
-	p("Index:     ", s.Index("test", "e"))
-	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
-	p("Repeat:    ", s.Repeat("a", 5))
-	p("Replace:   ", s.Replace("foo", "o", "0", -1))
-	p("Replace:   ", s.Replace("foo", "o", "0", 1))
-	p("Split:     ", s.Split("a-b-c-d-e", "-"))
-	p("ToLower:   ", s.ToLower("TEST"))
-	p("ToUpper:   ", s.ToUpper("test"))
+	p("Contains:  ", strings.Contains("test", "es"))
+	p("Count:     ", strings.Count("test", "t"))
+	p("HasPrefix: ", strings.HasPrefix("test", "te"))
+	p("HasSuffix: ", strings.HasSuffix("test", "st"))
+	p("Index:     ", strings.Index("test", "e"))
+	p("Join:      ", strings.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", strings.Repeat("a", 5))
+	p("Replace:   ", strings.Replace("foo", "o", "0", -1))
+	p("Replace:   ", strings.Replace("foo", "o", "0", 1))
+	p("Split:     ", strings.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", strings.ToLower("TEST"))
+	p("ToUpper:   ", strings.ToUpper("test"))
 }
