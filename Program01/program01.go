c811880f9b9478d71fd31f3283d1fa7eb2ec8d3f
kakaba2009 <_@example.com>
2023-04-15 09:19:29 +0900

    Parse HTML link and title

--- Program01/program01.go
+++ Program01/program01.go
@@ -20,52 +20,51 @@ func ParseTokens(resp *http.Response) {
 	defer writer.Flush()
 
 	tokenizer := html.NewTokenizer(resp.Body)
+	isTitle := false
+	isLink := false
 
 	for {
 		tokenType := tokenizer.Next()
 
 		switch tokenType {
 		case html.ErrorToken:
-			err := tokenizer.Err()
-			log.Fatalf("error %v", err)
-			break
-		case html.TextToken:
-			token := tokenizer.Token()
-			x := token.Data
-			if len(x) > 0 {
-				fmt.Println(x)
-				writer.Write([]string{"token", x})
-			}
+			fmt.Println("Finished")
+			return
 		case html.StartTagToken:
 			token := tokenizer.Token()
 			if "title" == token.Data {
-				tokenType = tokenizer.Next()
-				if tokenType == html.TextToken {
-					x := tokenizer.Token().Data
-					if len(x) > 0 {
-						fmt.Println(x)
-						writer.Write([]string{"title", x})
-					}
-				}
-			} else {
-				x := token.Data
-				if len(x) > 0 {
-					fmt.Println(x)
-					writer.Write([]string{"token", x})
-				}
+				isTitle = true
+			} else if "a" == token.Data {
+				isLink = true
+			}
+		case html.TextToken:
+			if isTitle {
+				ProcessText(tokenizer, writer)
+				isTitle = false
+			} else if isLink {
+				ProcessText(tokenizer, writer)
+				isLink = false
 			}
 		}
 	}
 }
 
-func ReadPage(link string) string {
+func ProcessText(tokenizer *html.Tokenizer, writer *csv.Writer) {
+	token := tokenizer.Token()
+	data := token.Data
+	if len(data) > 0 {
+		fmt.Println(data)
+		writer.Write([]string{data})
+	}
+}
+
+func ReadPage(link string) {
 	res, err := http.Get(link)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ParseTokens(res)
 	res.Body.Close()
-	return ""
 }
 
 func main() {
